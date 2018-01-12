50eb2559566bd8d08f6e84d90a29d3655bf81caf
Redenfloyd <floydcayanan12@gmail.com>
2018-01-12 22:49:41 +0800

    some enhancement

--- application/views/imports/vHeader.php
+++ application/views/imports/vHeader.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>DINE | Menu</title>
+    <title>DINE | Mameng's Seafood & BBQ Hauz</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css")?>">
